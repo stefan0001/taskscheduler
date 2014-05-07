ed1a210212170a3ee1f6418f54810568e62fe30b
MoritzKemp <moritz@kemp-thelen.de>
2014-05-07 19:16:38 +0200

    MoK - add method for creating issues related to a timetask

--- TaskScheduler/src/main/java/de/sep/innovativeoperation/taskscheduler/service/timetask/monitor/TimeTaskMonitor.java
+++ TaskScheduler/src/main/java/de/sep/innovativeoperation/taskscheduler/service/timetask/monitor/TimeTaskMonitor.java
@@ -9,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import de.sep.innovativeoperation.taskscheduler.dao.TimeTaskDAO;
+import de.sep.innovativeoperation.taskscheduler.model.data.IssueDraft;
+import de.sep.innovativeoperation.taskscheduler.model.data.IssueEntity;
+import de.sep.innovativeoperation.taskscheduler.model.data.IssueResolution;
+import de.sep.innovativeoperation.taskscheduler.model.data.IssueStatus;
 import de.sep.innovativeoperation.taskscheduler.model.data.TimeTask;
 import de.sep.innovativeoperation.taskscheduler.service.issueentity.IssueEntityDataService;
 import de.sep.innovativeoperation.taskscheduler.service.timetask.TimeTaskDataService;
@@ -39,11 +43,35 @@ public class TimeTaskMonitor {
 	
 	public void monitorTimTasks(){
 		List<TimeTask> timeTasks = timeTaskDAO.getTimeTaskWithNextFireTimeOlderThan(currentTime);
-		createIssue(timeTasks);
+		createIssues(timeTasks);
 	}
 	
-	private void createIssue(List<TimeTask> timeTasks){
-		Iterator<TimeTask> timeTaskiterator = timeTasks.iterator();
+	private void createIssues(List<TimeTask> timeTasks){
+		Iterator<TimeTask> timeTaskIterator = timeTasks.iterator();
+		
+		while(timeTaskIterator.hasNext()){
+			TimeTask currentTimeTask = timeTaskIterator.next();
+			createIssueEntityForTimeTask(currentTimeTask);
+		}
 	}
+	
+	private void createIssueEntityForTimeTask(TimeTask timeTask){
+		Iterator<IssueDraft> issueDraftIterator = timeTask.getIssueDrafts().iterator();
+		
+		//as long as there are IssueDrafts in the queue
+		while(issueDraftIterator.hasNext()){
+			IssueDraft currentIssueDraft = issueDraftIterator.next();
+			
+			//create and set up a new issueEntity for this issueDraft
+			IssueEntity newIssueEntity = new IssueEntity();
+			newIssueEntity.setId(0);
+			newIssueEntity.setIssueResolution(IssueResolution.UNRESOLVED);
+			newIssueEntity.setIssueStatus(IssueStatus.NEW);
+			
+			//let the service do the persistence thing
+			issueEntityService.createIssueEntity(currentIssueDraft.getId(), newIssueEntity);
+		}
+	}
+	
 	
 }
